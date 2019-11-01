8d9291c734b88569ff2ab15e0492afcce7ed09f6
jaylayson <46077327+jaylayson@users.noreply.github.com>
2019-11-01 17:01:08 +1000

    Update arrow-function2.js

--- arrowFunctions/arrow-function2.js
+++ arrowFunctions/arrow-function2.js
@@ -10,3 +10,23 @@ console.log(add(1,2))
 
 const add1 = () => arguments[0] + arguments[1]
 console.log(add1(1,2))
+
+/**
+ * 2. Arrow functions don't bind their 'this' value either
+ * 
+ * Lesson: When creating functions related to an object property, do NOT use Arrow functions.
+ */
+const car = {
+    color: 'Yellow',
+    getSummary: function(){
+        return `The car is ${this.color}`
+    }
+}
+console.log(car.getSummary())
+
+const car0 = {
+    color: 'Yellow',
+    getSummary: () => `The car is ${this.color}`
+    
+}
+console.log(car0.getSummary())
